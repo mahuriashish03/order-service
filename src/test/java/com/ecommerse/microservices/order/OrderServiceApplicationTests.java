4aa7192ca152867742c3846418162404d1824db5
mahuriashish03 <mahuriashish03@gmail.com>
2025-10-06 11:40:40 +0200

    Order-service: Database migration using Flyway, Integration test using RestAssured and test Container

--- src/test/java/com/ecommerse/microservices/order/OrderServiceApplicationTests.java
+++ src/test/java/com/ecommerse/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,57 @@
 package com.ecommerse.microservices.order;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer();
+
+	@LocalServerPort
+	private Integer port;
+
+	static {
+		mySQLContainer.start();
+	}
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+
+	}
 	@Test
-	void contextLoads() {
+	void createNewOrder() {
+		String submitOrder = """
+				{
+				    "skuCode": "IPhone 16",
+				    "price": 1500,
+				    "quantity": 1
+				}
+				""";
+
+		String responseString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitOrder)
+				.when()
+				.post("api/order")
+				.then()
+				.statusCode(201)
+				.extract()
+				.body().asString();
+		assertThat(responseString, Matchers.is("Order created successfully"));
 	}
 
 }
