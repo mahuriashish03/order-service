e823bf29e3eacb59f046e34f626e3d5b5a991a5b
mahuriashish03 <mahuriashish03@gmail.com>
2025-10-06 15:22:53 +0200

    Order-service: Synchronous communication between order service and inventory service using OpenFeign(Alrenative of RestTemplate, WebClient, RestClient).

--- src/main/java/com/ecommerse/microservices/order/service/OrderService.java
+++ src/main/java/com/ecommerse/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.ecommerse.microservices.order.service;
 
+import com.ecommerse.microservices.order.client.InventoryClient;
 import com.ecommerse.microservices.order.dto.OrderRequest;
 import com.ecommerse.microservices.order.model.Order;
 import com.ecommerse.microservices.order.repository.OrderRepository;
@@ -12,20 +13,27 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
-    public OrderService(OrderRepository orderRepository) {
+    public OrderService(OrderRepository orderRepository, InventoryClient inventoryClient) {
         this.orderRepository = orderRepository;
+        this.inventoryClient = inventoryClient;
     }
 
     public void placeOrder(OrderRequest orderRequest) {
 
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
+        boolean isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
+        if(isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
 
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product is not in stock" + orderRequest.skuCode());
+        }
     }
 }
---
+++ src/test/java/com/ecommerse/microservices/order/stubs/InventoryClientStup.java
@@ -0,0 +1,15 @@
+package com.ecommerse.microservices.order.stubs;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public class InventoryClientStup {
+
+    public static void stubInventoryCall(String skuCode, Integer quantity) {
+
+           stubFor(get(urlEqualTo("/api/inventory?skuCode=" + skuCode + "&quantity=" + quantity))
+            .willReturn(aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-type", "application/json")
+                    .withBody("true")));
+    }
+}
