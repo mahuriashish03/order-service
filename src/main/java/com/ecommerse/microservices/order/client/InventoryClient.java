8133b7e01047a6325743c9b07927cf206483c20f
mahuriashish03 <mahuriashish03@gmail.com>
2025-10-07 15:11:31 +0200

    Order-service: Cricuit breaker implmentation of microservice sync communication using resilience4j and actuator.

--- src/main/java/com/ecommerse/microservices/order/client/InventoryClient.java
+++ src/main/java/com/ecommerse/microservices/order/client/InventoryClient.java
@@ -1,11 +1,24 @@
 package com.ecommerse.microservices.order.client;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
-
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quatity, Throwable throwble) {
+        log.info("Cannot gte inventory for skuCode {}, failure reason: {}", code, throwble.getMessage());
+        return false;
+    }
 }
